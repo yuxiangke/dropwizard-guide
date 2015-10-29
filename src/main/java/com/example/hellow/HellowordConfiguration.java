98f26cec6436e88dcb46a54c8711fb07406cf09c
xiangke.yu <120571267@qq.com>
2015-10-29 15:48:40 +0800

    initial commit

--- src/main/java/com/example/hellow/HellowordConfiguration.java
+++ src/main/java/com/example/hellow/HellowordConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.hellow;
 
+import javax.validation.Valid;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,24 +12,38 @@ public class HellowordConfiguration extends Configuration {
 	@NotEmpty
 	private String template;
 	@NotEmpty
-	private String defaultName="Stranger";
+	private String defaultName = "Stranger";
+
+	@Valid
+	private TestConfiguration test;
+
 	@JsonProperty
 	public String getTemplate() {
 		return template;
 	}
+
 	@JsonProperty
 	public void setTemplate(String template) {
 		this.template = template;
 	}
+
 	@JsonProperty
 	public String getDefaultName() {
 		return defaultName;
 	}
+
 	@JsonProperty
 	public void setDefaultName(String defaultName) {
 		this.defaultName = defaultName;
 	}
-	
-	
+
+	@JsonProperty
+	public TestConfiguration getTest() {
+		return test;
+	}
+
+	public void setTest(TestConfiguration test) {
+		this.test = test;
+	}
 
 }
---
+++ src/main/java/com/example/hellow/TestConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.hellow;
+
+import javax.validation.constraints.NotNull;
+
+public class TestConfiguration {
+	@NotNull
+	private String name;
+	@NotNull
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
